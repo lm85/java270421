b232e3dc3e81d81c3112c441014b19fd68617ac7
lmasic <masopust@spsejecna.cz>
2021-06-10 19:16:43 +0200

    Add files via upload
    Otáčení karet

--- Pexeso/src/pexeso/GameButton.java
+++ Pexeso/src/pexeso/GameButton.java
@@ -11,12 +11,25 @@ import javax.swing.JButton;
  *
  * @author lmasic
  */
-public class GameButton extends JButton{
-    private String symbol=null;
+public class GameButton extends JButton {
 
-    public GameButton() {
-    super();
-            
+    private String symbol = "";
+    private int index = -1;
+    static int CoutTurnedCard = 0;
+    static int[] turnedCard = new int[2];
+
+    public GameButton(int index) {
+        super();
+        this.index = index;
+
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
     }
 
     public String getSymbol() {
@@ -27,5 +40,22 @@ public class GameButton extends JButton{
         this.symbol = symbol;
     }
 
-    
+    public boolean turnCard() {
+
+        if (super.getText().equals("PEXESO")) {
+            if (CoutTurnedCard == 2) {
+                
+                return true;
+            }
+
+            super.setText(symbol);
+
+            CoutTurnedCard++;
+        } else {
+
+            super.setText("PEXESO");
+            CoutTurnedCard--;
+        }
+        return false;
+    }
 }
--- Pexeso/src/pexeso/GameField.java
+++ Pexeso/src/pexeso/GameField.java
@@ -4,49 +4,63 @@
  * and open the template in the editor.
  */
 package pexeso;
+
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+
 /**
  *
  * @author lmasic
  */
-
 public class GameField extends Thread{
-   JFrame frame; 
-    public GameField() {
+
+    JFrame frame;
+    GameButton[] buttons;
+    String[] symbols;
     
+
+    public GameField() {
+
         frame = new JFrame("Piškvorky");
         /* int r = Integer.parseInt(JOptionPane.showInputDialog(frame, "Jak mám být velké pole?"));
         if (r < 3) {
             r = 3;
         }*/
-        int r=4;
+        int r = 4;
+        symbolsGen(r*r);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setMinimumSize(new Dimension(r*100+20, r*100+20));
-       Container pane = frame.getContentPane();
+        frame.setMinimumSize(new Dimension(r * 100 + 20, r * 100 + 20));
+        Container pane = frame.getContentPane();
         pane.setLayout(new GridLayout(r, r));
 
-       GameButton[] buttons = new GameButton[r * r];
+        buttons = new GameButton[r * r];
         
         for (int i = 0; i < buttons.length; i++) {
-            buttons[i] = new GameButton();
+            buttons[i] = new GameButton(i);
             buttons[i].setText("PEXESO");
+            buttons[i].setSymbol(symbols[i]);
+            //buttons[i].setText(symbols[i]);    
             
             buttons[i].addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
-                   
-                   
+                    GameButton gb = (GameButton) e.getSource();
+
+                            if (gb.turnCard()) {//hraje další hráč
+                            
+                            
+                            }
+                            
                     
-                   
                 }
             });
 
@@ -57,7 +71,26 @@ public class GameField extends Thread{
         start();
     }
     
+    
+    
+    private void symbolsGen(int r) {
+        
+        symbols = new String[r];
+         for (int i = 0; i < symbols.length/2; i++) {
+            symbols[i]=Integer.toString(i+1);
+            symbols[symbols.length-1-i]=Integer.toString(i+1);
+                    }
+         
+         Random rand = new Random();
+		
+		for (int i = 0; i < symbols.length; i++) {
+			int randomIndexToSwap = rand.nextInt(symbols.length);
+			String temp = symbols[randomIndexToSwap];
+			symbols[randomIndexToSwap] = symbols[i];
+			symbols[i] = temp;
+		}
+         
+         
+    }
 
-   
 }
-
