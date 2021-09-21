79be2acfc454509daf9e0bf50fa6af7d00da9dfb
lm85 <30482258+lm85@users.noreply.github.com>
2021-09-21 19:00:59 +0200

    Add files via upload
    Třída Customers

---
+++ CRM/src/Customers.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Customers {
+    private ArrayList<Customer> Customers = new ArrayList<>();
+
+    public Customers() {
+    }
+
+    public void addCustomer(Customer customer) {
+
+        //TODO: řešit duplicity
+        this.Customers.add(customer);
+
+    }
+
+    public void removeCustomer(Customer customer) {
+
+
+
+    }
+
+    public void removeCustomer(int index) {
+
+    
+
+    }
+
+    public Customer getCustomer(int index) {
+
+
+
+    }
+
+    public Customer searchCustomer(String name) {
+
+
+
+    }
+
+
+
+}
